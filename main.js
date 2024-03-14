a8e466744c65c7e2e2988eab2cf23d44bf778a8b
Ka1quee <kaiqueoliveira375@gmail.com>
2024-03-14 00:39:22 -0300

    Adicionando responsividade

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var btnMenu = document.querySelector('#menu');
 var menuLateral = document.querySelector('.nav-lateral');
 var menuLink = document.querySelector('.menu-link');
-// Usar querySelector em vez de querySelectorAll para selecionar o elemento com a classe '.teste'
 var menus = document.querySelector('.container-menu');
+var conteudoPagina = document.querySelector('.conteudo-pagina')
 
 btnMenu.addEventListener('click', toggleMenu);
 
@@ -14,11 +14,13 @@ function toggleMenu() {
       menuLink.classList.add('menu-item');
       // Adiciona a classe '.sumir' ao único elemento com a classe '.teste'
       menus.classList.add('hidden');
+      conteudoPagina.classList.add('mover-conteudo')
     } else {
       menuLateral.classList.remove('nav-lateral-hidden');
       menuLink.classList.remove('menu-item');
       // Remove a classe '.sumir' do único elemento com a classe '.teste'
       menus.classList.remove('hidden');
+      conteudoPagina.classList.remove('mover-conteudo')
     }
     
     // Atualiza o estado do menu
